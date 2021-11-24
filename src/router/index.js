e7fef7faac0aefa242ca9ddb6d85e884492ed1e7
Vadim <vaspean@mail.ru>
2021-11-24 11:55:34 +0300

    base layout add

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,60 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue'),
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue'),
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue'),
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue'),
   }
 ]
 
