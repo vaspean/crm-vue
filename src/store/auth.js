abe97c55f434ca80bdd4107e0c11dcac38e33083
Vadim <vaspean@mail.ru>
2021-11-26 15:37:09 +0300

    register added

--- src/store/auth.js
+++ src/store/auth.js
@@ -7,8 +7,29 @@ export default {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password)
       } catch (error) {
+        commit('setError', error)
         throw error
       }
+    },
+    async register({dispatch, commit},{email, password, name}) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        const uid = await dispatch('getUid');
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          bill: 10000,
+          name: name
+        })
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    },
+    getUid(){
+      const user = firebase.auth().currentUser;
+      return user? user.uid : null
+    },
+    async logout() {
+      await firebase.auth().signOut();
     }
   }
 }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -91,7 +91,7 @@ export default {
     agree: {checked: value => value}
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -101,8 +101,15 @@ export default {
         password: this.password,
         name: this.name
       }
-      console.log(formData)
-      this.$router.push('/')
+
+      try {
+        await this.$store.dispatch('register', formData);
+        this.$router.push('/')
+      } catch (e) {
+
+      }
+      // console.log(formData)
+      
     }
   }
 }
